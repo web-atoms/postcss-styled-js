443a1b459603b99a6a78f8c1de770a995f968433
Akash Kava <39438041+ackava@users.noreply.github.com>
2025-04-02 11:05:00 +0530

    Added css

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 const all = require("./dist/postcss-js");
+const styled = require("./dist/styled");
+all.default.css = styled.default ? styled.default.css : styled.css;
 module.exports = all.default;
